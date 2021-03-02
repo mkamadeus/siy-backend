12d9a601823773ac9a377122962c258400c2129f
Difa Habiba Rahman <difa.habiba.rahman@gmail.com>
2021-03-02 11:33:16 +0700

    Move LO weights to Class.

--- src/entity/Teaches.ts
+++ src/entity/Teaches.ts
@@ -1,6 +1,5 @@
 import{
     BaseEntity,
-    Column,
     Entity,
     PrimaryGeneratedColumn,
     ManyToOne,
@@ -10,30 +9,9 @@ import Teacher from "./Teacher";
 import Class from "./Class";
 
 @Entity()
-export default class LOSupplement extends BaseEntity{
+export default class Teaches extends BaseEntity{
     @PrimaryGeneratedColumn()
     id : number;
-
-    @Column({name : "lo_a_weight"})
-    loAWeight : number;
-
-    @Column({name : "lo_b_weight"})
-    loBWeight : number;
-
-    @Column({name : "lo_c_weight"})
-    loCWeight : number;
-
-    @Column({name : "lo_d_weight"})
-    loDWeight : number;
-
-    @Column({name : "lo_e_weight"})
-    loEWeight : number;
-
-    @Column({name : "lo_f_weight"})
-    loFWeight : number;
-
-    @Column({name : "lo_g_weight"})
-    loGWeight : number;
 
     @ManyToOne(() => Teacher, (teacher) => teacher.id)
     @JoinColumn({ name: "teacher_id" })
