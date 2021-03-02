388fefef45bcf55fabfa9cd81bdcfcd955d86a0d
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2021-03-02 17:09:44 +0700

    LO Service added

---
+++ src/services/LOSupplementService.ts
@@ -0,0 +1,41 @@
+import LOSupplement from "@/entity/LOSupplement";
+import { Service } from "typedi";
+import { getRepository, Repository } from "typeorm";
+
+@Service()
+export default class LOSupplementService {
+  private loSupplementRepository: Repository<LOSupplement> = getRepository(
+    LOSupplement
+  );
+
+  public async getAll(): Promise<LOSupplement[]> {
+    return await this.loSupplementRepository
+      .find()
+      .then((loSupplements) => loSupplements);
+  }
+
+  public async getOne(id: number): Promise<LOSupplement> {
+    return await this.loSupplementRepository
+      .findOne({ where: { id } })
+      .then((loSupplement) => loSupplement);
+  }
+
+  public async create(loSupplement: LOSupplement): Promise<LOSupplement> {
+    const lo = await this.loSupplementRepository.save(loSupplement);
+    return await this.getOne(lo.id);
+  }
+
+  public async update(
+    id: number,
+    loSupplement: LOSupplement
+  ): Promise<LOSupplement> {
+    loSupplement.id = id;
+    const lo = await this.loSupplementRepository.save(loSupplement);
+    return await this.getOne(lo.id);
+  }
+
+  public async delete(id: number): Promise<void> {
+    await this.loSupplementRepository.delete(id);
+    return;
+  }
+}
