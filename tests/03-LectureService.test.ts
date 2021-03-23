3143bd6b8b74844b6e8b8f7c4effae13f6604949
Difa Habiba Rahman <difa.habiba.rahman@gmail.com>
2021-03-23 10:14:32 +0700

    Create test file for lecture

---
+++ tests/03-LectureService.test.ts
@@ -0,0 +1,81 @@
+import "reflect-metadata";
+import Lecture from "@/entity/Lecture";
+import { typeormLoader } from "@/loaders/typeormLoader";
+import { LectureService } from "@/services/LectureService";
+import { expect } from "chai";
+import { Container } from "typedi";
+import { Connection } from "typeorm";
+
+let connection: Connection;
+before(async () => {
+  connection = await typeormLoader();
+});
+
+describe("LectureService test", () => {
+  describe("#1 Lecture creation", () => {
+    let lecId: number;
+    it("should have 0 lecture", (done) =>{
+      const lectureService = Container.get(LectureService);
+      lectureService
+        .getAll()
+        .then((lectureArray)=>{
+          expect(lectureArray.length).to.be.equal(0);
+          done();
+        });
+    });
+    it("should create 1 lecture", (done) =>{
+      const lectureService = Container.get(LectureService);
+      lectureService
+        .create({
+          courseId: 1,
+          semester: 3,
+          year: 2021,
+        } as Lecture)
+        .then((lecture) =>{
+          lecId = lecture.id;
+          expect(lecture.courseId).to.be.equal(1);
+          expect(lecture.semester).to.be.equal(3);
+          expect(lecture.year).to.be.equal(2021);
+          expect(lecture.loAPracticumWeight).to.be.equal(0);
+          return lectureService.getAll();
+        })
+        .then((lectureArray) =>{
+          expect(lectureArray.length).to.be.equal(1);
+          done();
+        });
+    });
+    it("should update lecture loAPracticum Weight to 3, semester to 4", (done) => {
+      const lectureService = Container.get(LectureService);
+      lectureService
+        .update(lecId, {
+          courseId: 1,
+          semester: 4,
+          year: 2021,
+          loAPracticumWeight: 3,
+        } as Lecture)
+        .then((lecture) =>{
+          expect(lecture.courseId).to.be.equal(1);
+          expect(lecture.semester).to.be.equal(4);
+          expect(lecture.year).to.be.equal(2021);
+          expect(lecture.loAPracticumWeight).to.be.equal(3);
+          done();
+        });
+    });
+    it("should delete the lecture entry", (done) =>{
+      const lectureService = Container.get(LectureService);
+      lectureService
+        .delete(lecId)
+        .then(() => {
+          return lectureService.getAll();
+        })
+        .then((lectureArray) => {
+          expect(lectureArray.length).to.be.equal(0);
+          done();
+        });
+    });
+  });
+});
+
+after(() => {
+  connection.close();
+});
