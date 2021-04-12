3a90d3f1146af9c9171e2d57ec43b6876ae2ab77
Difa Habiba Rahman <difa.habiba.rahman@gmail.com>
2021-04-12 10:27:01 +0700

    Create basic service for RatingQuestionnaire

---
+++ src/services/RatingQuestionnaireService.ts
@@ -0,0 +1,40 @@
+import RatingQuestionnaire from "@/entity/RatingQuestionnaire";
+import { Service } from "typedi";
+import { getRepository, Repository } from "typeorm";
+
+@Service()
+export class RatingQuestionnaireService{
+    private ratingQuestionnaireRepository: Repository<RatingQuestionnaire> = getRepository(
+        RatingQuestionnaire,
+        process.env.NODE_ENV === "test" ? "test" : "default"
+    );
+
+    public async getAll(): Promise<RatingQuestionnaire[]>{
+        return await this.ratingQuestionnaireRepository.find().then((rq) => rq);
+    }
+
+    public async getOne(id: number): Promise<RatingQuestionnaire>{
+        return await this.ratingQuestionnaireRepository
+          .findOne({where: {id}})
+          .then((rq) => rq);
+    }
+
+    /*TODO: get Teaches?
+            get ratings
+            get m_x rating */
+
+    public async create(rq: RatingQuestionnaire): Promise<RatingQuestionnaire>{
+        return await this.ratingQuestionnaireRepository.save(rq);
+    }
+
+    public async update(id: number, rq: RatingQuestionnaire): Promise<RatingQuestionnaire>{
+        rq.id = id;
+        await this.ratingQuestionnaireRepository.update(id, rq);
+        return await this.getOne(id);
+    }
+
+    public async delete(id: number): Promise<void>{
+        await this.ratingQuestionnaireRepository.delete(id);
+        return;
+    }
+}
